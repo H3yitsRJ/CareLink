2c3654fad8903142cf35a50291b8f9289973a4b2
RJ Vogel <rowan.jayce13@gmail.com>
2026-09-02 19:37:59 -0700

    Bottom Navigation Bar created to be implemented on screens. No functionality yet.

--- app/src/main/java/navigation/BottomNavBar.kt
+++ app/src/main/java/navigation/BottomNavBar.kt
@@ -1,2 +1,58 @@
 package navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BottomNavBar() {
+
+    NavigationBar {
+
+        NavigationBarItem(selected = false,
+        onClick = { },
+        icon = {
+            Icon(Icons.Default.Home, "Home")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(Icons.Default.Star, "Medication")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(Icons.Default.DateRange, "Calendar")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(Icons.Default.List, "Tasks")
+            }
+        )
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { },
+            icon = {
+                Icon(Icons.Default.Person, "Profile")
+            }
+        )
+    }
+}
